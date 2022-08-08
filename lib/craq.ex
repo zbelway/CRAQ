2a2b90a9bbb695275d18eb1f67f8a324e9a34eb2
zbelway <zbelway@gmail.com>
2022-08-08 05:16:51 -0700

    added lib

---
+++ lib/craq.ex
@@ -0,0 +1,74 @@
+defmodule Craq do
+  @moduledoc """
+  Documentation for `Craq`.
+  """
+@type results :: %{
+questions_index: integer(),
+error_message: map(),
+terminus: boolean(),
+answer: map()
+}
+
+def generate(questions, answers) do
+  run_answer(questions, answers)
+end
+
+def run_answer(questions, answer) do
+  results = %{
+    question_index: 0,
+    error_message: %{},
+    terminus: false,
+    answer: answer
+  }
+  Enum.each(questions, fn question -> run_question(question, results) end)
+  if Enum.empty?(results.error_message) do
+    IO.puts("passed!")
+  else
+    IO.puts(results.error_message)
+  end
+end
+
+def run_question(question, results) do
+  {answer_choice, results} = get_answer_tuple(results)
+  question_option = get_question_option(question.options, answer_choice)
+  check_for_errors(question_option, results)
+  Map.put(results, :question_index, results.question_index + 1)
+end
+
+@spec get_answer_tuple(results()) :: tuple()
+def get_answer_tuple(results) do
+   ans = Map.get(results.answer, String.to_atom("q#{results.question_index}"))
+   {ans, results}
+end
+
+def check_for_errors(question_option, results) do
+  error = false
+
+  if results.terminus == true do
+    error = "was answered even though a previous response idicated that the questions were complete"
+  end
+
+  case question_option do
+    nil -> error = "has an answer that is not on the list of valid answers"
+    :blank_answer -> error = "was not answered"
+    other_option -> results =
+      if Map.get(question_option, :complete_if_selected) do
+        Map.put(results, :terminus, true)
+      end
+  end
+
+  if error do
+    Map.put(results.error_message, String.to_atom("q#{results.questions_index}"), error)
+  end
+  error
+end
+
+def get_question_option(_, :blank_answer) do
+  :blank_answer
+end
+
+def get_question_option(options, answer_choice) do
+  Enum.at(options, answer_choice)
+end
+
+end
---
+++ lib/questions.ex
@@ -0,0 +1,49 @@
+defmodule Questions do
+  @moduledoc """
+    This are the CRAQ questions that will be asked.
+  """
+@type text :: String.t()
+
+@type option :: %{
+  text: text(),
+  complete_if_selected: boolean() | nil
+}
+
+@type question :: %{
+  text: text(),
+  option: option()
+}
+
+@type questions() :: list(question())
+
+@spec questions :: questions()
+def questions do
+[
+  %{options: [
+    %{text: "No"},
+    %{text: "how can anyone really know?", complete_if_selected: true}
+  ],
+  text: "Are you a human?"
+  },
+  %{options: [
+    %{text: "No, why would I want to be different?"},
+    %{text: "I want to be a real boy!"}
+  ],
+  text: "Do you wish you were?"
+  }
+]
+end
+
+def answers do
+[
+  %{},
+  %{q0: 1},
+  %{q0: 0, q1: 1},
+  %{q0: 1, q1: 0},
+  %{q0: 2, q1: 0},
+  %{q0: 0, q1: 1, q2: 1},
+  %{q1: 0, q2: 2}
+]
+end
+
+end
